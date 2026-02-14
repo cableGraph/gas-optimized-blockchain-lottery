9d4d18e96cf006ff06603bfecd1acc9364fd394c
Denniskiptoo <denniskiptoo034@gmail.com>
2026-02-14 11:10:09 +0300

    Clean up comments in Raffle.sol
    Removed layout comments from Raffle.sol

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,23 +1,4 @@
-// Layout
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
 
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
 
 //SPDX-Licence-Identifier: MIT
 
